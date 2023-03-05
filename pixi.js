e3563e712da4ec1cd9dfe92d8cb701a61f15193c
moise10r <moiserushanika2006@gmail.com>
2023-03-05 22:29:22 +0200

    draw a rectangle ramdomly on a canvas in ticker function

--- pixi.js
+++ pixi.js
@@ -34,6 +34,24 @@ torus.beginFill(0x66CCFF);
 torus.drawTorus(100, 700, 80, 100);
 torus.endFill();
 
+
+// draw a star
+const star = new Graphics();
+star.beginFill(0x66CCFF);
+star.drawStar(400, 400, 5, 60, 100);
+star.endFill();
+
+app.stage.addChild(star);
+
+// draw a rectangle ramdomly on a canvas in ticker function
+// app.ticker.add((delta) => {
+//     const rectangle = new Graphics();
+//     rectangle.beginFill(0x66CCFF)
+//     .drawRect(Math.random() * app.screen.width, Math.random() * app.screen.height, 10, 10)
+//     .endFill();
+//     app.stage.addChild(rectangle);
+// });
+
 app.stage.addChild(torus);
 app.stage.addChild(circle);
 app.stage.addChild(rectangle);
