ee4e8ce61b4346af8bb753b5c8ae59c614bcc410
moise10r <moiserushanika2006@gmail.com>
2023-03-05 22:09:57 +0200

    Add a retangle and the circle to a pixi canvas.

--- pixi.js
+++ pixi.js
@@ -1,11 +1,33 @@
 const Application = PIXI.Application;
 
 const app = new Application({
-    width: document.body.clientWidth,
-    height: 900,
+    width: 800,
+    height: 800,
     antialias: true,
     transparent: false,
-    resolution: 1
 });
-
 document.body.appendChild(app.view);
+
+app.renderer.backgroundColor = 0x061639;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+app.view.style.position = "absolute";
+const Graphics = PIXI.Graphics;
+const rectangle = new Graphics();
+rectangle.beginFill(0x66CCFF)
+.drawRect(200, 200, 100, 120)
+.endFill();
+
+// draw a circle
+const circle = new Graphics();
+circle.beginFill(0x66CCFF);
+circle.drawCircle(0, 0, 32);
+circle.endFill();
+
+// position the circle sprite
+// circle.x = 250;
+// circle.y = 250;
+
+app.stage.addChild(circle);
+app.stage.addChild(rectangle);
+
+app.stage.addChild(rectangle);
